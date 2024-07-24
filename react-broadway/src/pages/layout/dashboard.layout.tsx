4c1e8830092cc0fd60f05251a06b45f69f8ed4ed
Krishna Tiwari <61178259+krishna-cyber@users.noreply.github.com>
2024-07-24 22:48:22 +0545

    notification label

--- react-broadway/src/pages/layout/dashboard.layout.tsx
+++ react-broadway/src/pages/layout/dashboard.layout.tsx
@@ -1,6 +1,6 @@
 import { Avatar, Dropdown, Navbar,Badge, Sidebar } from "flowbite-react";
 import logo from '../../assets/images/logo/logo-only.png'
-import { HiBell, HiCurrencyDollar, HiLogout, HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards } from "react-icons/hi";
+import { HiBell, HiCurrencyDollar, HiLogout, HiArrowSmRight, HiChartPie, HiInbox, HiShoppingBag, HiTable, HiUser, HiViewBoards, HiX } from "react-icons/hi";
 import { HiCircleStack, HiSquare3Stack3D, HiSquaresPlus } from "react-icons/hi2";
 import { useState } from "react";
 
@@ -99,13 +99,13 @@ export const AdminPanel = ()=>{
           <Sidebar.Item href="#" icon={HiViewBoards}>
             Kanban
           </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiInbox}>
+          <Sidebar.Item href="#" icon={HiInbox} label="3" labelColor="green">
             Inbox
           </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiUser}>
+          <Sidebar.Item href="#" icon={HiUser} label="500" labelColor="pink">
             Users
           </Sidebar.Item>
-          <Sidebar.Item href="#" icon={HiShoppingBag}>
+          <Sidebar.Item href="#" icon={HiShoppingBag} label={"25256"} labelColor="purple">
             Products
           </Sidebar.Item>
           <Sidebar.Item href="#" icon={HiArrowSmRight}>
@@ -117,34 +117,22 @@ export const AdminPanel = ()=>{
         </Sidebar.ItemGroup>
       </Sidebar.Items>
       <Sidebar.CTA>
-        <div classNameName="mb-3 flex items-center">
-          <Badge color="warning">Beta</Badge>
+        <div className="mb-3 flex items-center">
+          <Badge color="yellow">Beta</Badge>
           <button
             aria-label="Close"
-            classNameName="-m-1.5 ml-auto inline-flex h-6 w-6 rounded-lg bg-gray-100 p-1 text-cyan-900 hover:bg-gray-200 focus:ring-2 focus:ring-gray-400 dark:bg-gray-700 dark:text-gray-400 dark:hover:bg-gray-600"
+            className="-m-1.5 ml-auto inline-flex h-6 w-6 rounded-lg bg-gray-100 p-1 text-cyan-900 hover:bg-gray-200 focus:ring-2 focus:ring-gray-400 dark:bg-gray-700 dark:text-gray-400 dark:hover:bg-gray-600"
             type="button"
           >
-            <svg
-              aria-hidden
-              classNameName="h-4 w-4"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
+          <HiX />
           </button>
         </div>
-        <div classNameName="mb-3 text-sm text-cyan-900 dark:text-gray-400">
+        <div className="mb-3 text-sm text-cyan-900 dark:text-gray-400">
           Preview the new Flowbite dashboard navigation! You can turn the new navigation off for a limited time in your
           profile.
         </div>
         <a
-          classNameName="text-sm text-cyan-900 underline hover:text-cyan-800 dark:text-gray-400 dark:hover:text-gray-300"
+          className="text-sm text-cyan-900 underline hover:text-cyan-800 dark:text-gray-400 dark:hover:text-gray-300"
           href="#"
         >
           Turn new navigation off
