5d6f992d3fc7d04ae59b913b545b947bad11c166
Krishna Tiwari <61178259+krishna-cyber@users.noreply.github.com>
2024-08-23 07:43:28 +0545

    Admin Dashboart chart visualization

--- react-broadway/src/pages/dashboard/admin.dashboard.page.tsx
+++ react-broadway/src/pages/dashboard/admin.dashboard.page.tsx
@@ -108,7 +108,78 @@ const AdminDashboard = () => {
       },
     },
   };
-
+  const monthlySellingConfig = {
+    data: [
+      {
+          "month": "January",
+          "salesAmount": 3520
+      },
+      {
+          "month": "February",
+          "salesAmount": 2940
+      },
+      {
+          "month": "March",
+          "salesAmount": 4780
+      },
+      {
+          "month": "April",
+          "salesAmount": 4253
+      },
+      {
+          "month": "May",
+          "salesAmount": 12702
+      },
+      {
+          "month": "June",
+          "salesAmount": 2288
+      },
+      {
+          "month": "July",
+          "salesAmount": 2015
+      },
+      {
+          "month": "August",
+          "salesAmount": 6094
+      },
+      {
+          "month": "September",
+          "salesAmount": 5320
+      },
+      {
+          "month": "October",
+          "salesAmount": 4810
+      },
+      {
+          "month": "November",
+          "salesAmount": 3590
+      },
+      {
+          "month": "December",
+          "salesAmount": 6890
+      }
+  ]
+  
+  ,
+  xField: 'month',
+  yField: 'salesAmount',
+  label: {
+    text: (d) => `${d.salesAmount}`,
+    textBaseline: 'bottom',
+  },
+  axis: {
+    y: {
+      labelFormatter: '.0f',
+    },
+  },
+  style: {
+    // Rounded corners style
+    radiusTopLeft: 10,
+    radiusTopRight: 10,
+  
+  },
+  
+};
 
 
   return (
@@ -181,33 +252,19 @@ const AdminDashboard = () => {
         </h2></div>
         <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72">
         <h2 className="text-semibold p-4 text-2xl">
-          Top Sellling products this month 
+          Top 5 brands by selling 
+          <Column {...monthlySellingConfig} />
         </h2>
         </div>
-        <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72">
-          8
-        </div>
-        <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72">
-          9
-        </div>
+      
       </div>
-      <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-96 mb-4">
-        10
+      <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-fit mb-4">
+      <h2 className="text-semibold p-4 text-2xl">
+         MonthWise Selling
+        </h2>
+          <Column {...monthlySellingConfig} />
       </div>
-      <div className="grid grid-cols-2 gap-4">
-        <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72">
-          11
-        </div>
-        <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72">
-          12
-        </div>
-        <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72">
-          13
-        </div>
-        <div className="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-48 md:h-72">
-          14
-        </div>
-      </div>
+    
     </>
   );
 };
